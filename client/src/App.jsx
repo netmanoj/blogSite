fcd890a184d7ffb0773f59cd72fdcbc480f8e32b
manoj-adhikari <imadhikarimanoj@gmail.com>
2024-08-21 21:47:06 +0530

    origin main

---
+++ client/src/App.jsx
@@ -0,0 +1,88 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          blogSite
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://static.addtoany.com/images/dracaena-cinnabari.jpg"></img>
+        </div>
+        <div className="texts">
+          <h2>What is Lorem Ipsum?</h2>
+          <p className="info">
+            <a className="author">Manoj Adhikari</a>
+            <time>2024-01-06</time>
+          </p>
+          <p className="summary">
+            orem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. lolla
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://static.addtoany.com/images/dracaena-cinnabari.jpg"></img>
+        </div>
+        <div className="texts">
+          <h2>What is Lorem Ipsum?</h2>
+          <p className="info">
+          <a className="author">Manoj Adhikari</a>
+            <time>2024-01-06</time>
+          </p>
+          <p className="summary">
+            orem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. lolla
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://static.addtoany.com/images/dracaena-cinnabari.jpg"></img>
+        </div>
+        <div className="texts">
+          <h2>What is Lorem Ipsum?</h2>
+          <p className="info">
+          <a className="author">Manoj Adhikari</a>
+            <time>2024-01-06</time>
+          </p>
+          <p className="summary">
+            orem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. lolla
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
